8c96238441cdcfdb96a60de142b2c18aab412e90
KilroyWasHere-cs-j <59028838+KilroyWasHere-cs-j@users.noreply.github.com>
2022-06-13 18:12:15 -0400

    Update Shooter.java

--- src/main/java/org/frc5687/swerve/subsystems/Shooter.java
+++ src/main/java/org/frc5687/swerve/subsystems/Shooter.java
@@ -4,11 +4,16 @@ import com.ctre.phoenix.motorcontrol.can.TalonFX;
 
 import org.frc5687.swerve.RobotMap;
 
+import edu.wpi.first.math.controller.PIDController;
+
 public class Shooter {
 
     private TalonFX _shooter;
+    private PIDController _controller;
     
     public Shooter(){
         _shooter = new TalonFX(RobotMap.CAN.TALONFX.SHOOTER);
     }
+
+
 }
