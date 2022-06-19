81b88a0ea6f4992c5d4c5e6d9c2b7c8bbeae2a51
KilroyWasHere-cs-j <59028838+KilroyWasHere-cs-j@users.noreply.github.com>
2022-06-19 15:44:03 -0400

    Update Intake.java

--- src/main/java/org/frc5687/swerve/subsystems/Intake.java
+++ src/main/java/org/frc5687/swerve/subsystems/Intake.java
@@ -2,8 +2,6 @@ package org.frc5687.swerve.subsystems;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonFX;
-
-import org.frc5687.swerve.RobotMap;
 import org.frc5687.swerve.Constants.INTAKE;
 import org.frc5687.swerve.RobotMap.CAN.TALONFX;
 import org.frc5687.swerve.util.OutliersContainer;
